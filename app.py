f6eebfc5bc768d99d0ec2d4034a43516d14ea4bf
ubu18-kanji <smile.smiling01@gmail.com>
2019-06-16 22:16:56 +0900

    add logger

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import yaml
 from database import controller
 import inform
 
+import logger
+
 CONFIG_PATH = "./config.yaml"
 
 
@@ -12,6 +14,8 @@ def main():
     # read config
     with open(CONFIG_PATH) as file:
         config = yaml.load(stream=file, Loader=yaml.SafeLoader)
+
+    log = logger.Logger(__name__)
 
     slack_inform = inform.SlackInform(config["slack"][0]["webhook_url"])
 
@@ -23,9 +27,11 @@ def main():
         database_controller = controller.Controller(app_id, app_name, app_country)
         if not database_controller.check_existence_review_database():
             database_controller.create_review_database()
+            log.logger.info("create {} database".format(app_name))
 
         latest_review_list = database_controller.check_latest_reviews()
         if not latest_review_list:
+            log.logger.info("not {} latest reviews".format(app_name))
             continue
 
         for latest_review in latest_review_list:
@@ -33,6 +39,7 @@ def main():
             slack_inform.inform(attachments)
 
         database_controller.update_review_database(latest_review_list)
+
 
 if __name__ == "__main__":
     main()
